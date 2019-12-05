07bb5ca44065f60245f39a45bf334a145803f95c
XiaoBing <xiaobing@qutoutiao.net>
2019-12-05 19:18:50 +0800

    update

--- test/native_ability_test.dart
+++ test/native_ability_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:native_ability/native_ability.dart';
+import '../lib/native_ability.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('native_ability');
